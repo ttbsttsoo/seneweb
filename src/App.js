3bb9e8ec0e8ac96f7672b8c7f5268365dbc92584
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-12-11 03:18:42 +0000

    Materialize installed successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
+import { Button, Icon } from "react-materialize";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>Testing React Installed OK</p>
+        <div>
+          <Button>Material</Button>
+        </div>
+        <Button floating className="pink lighten-2 z-depth-2" icon="favorite" />
+        <p>OK</p>
       </div>
     );
   }
