b5397eaa7d5ad9b713aac02729510873230e743a
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-12-13 05:26:20 +0000

    Change PageDAcceuil class to function

--- src/pageDAcceuil/senewebPageDAcceuil.js
+++ src/pageDAcceuil/senewebPageDAcceuil.js
@@ -3,16 +3,16 @@ import React, { Component } from "react";
 import { Row, Col } from "react-materialize";
 import Article from "../article/senewebArticle";
 
-class PageDAcceuil extends Component {
-  render() {
-    return (
-      <Row>
-        <Col s={4}>
-          <Article id={2} />
-        </Col>
-      </Row>
-    );
-  }
-}
+const PageDAcceuil = ({ article_ids }) => {
+  return (
+    <Row>
+      <Col s={4}>
+        <Article id={2} />
+      </Col>
+    </Row>
+  );
+};
+
+// const mapStateToProps
 
 export default PageDAcceuil;
