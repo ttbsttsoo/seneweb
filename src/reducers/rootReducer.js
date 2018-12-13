1b98c4dab0799b841713a1e16c1bd53b466b71b4
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-12-13 02:11:49 +0000

    Setting up redux

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,24 @@
+import ArticleImage from "../img/mane.jpg";
+
+const initState = {
+  articles: [
+    {
+      id: 1,
+      contenu:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sed veritatis aspernatur ",
+      image: ArticleImage,
+      titre: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      topic: "Sport",
+      date: "9h",
+      reactions: {
+        ncommentaires: 2
+      }
+    }
+  ]
+};
+
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+export default rootReducer;
