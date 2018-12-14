780e4003c3716701bd0397c451703f0de9e5eec7
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-12-14 06:13:55 +0000

    Added simple commentaires component

--- src/pageDetailsArticle/commentaires/senewebCommentaires.js
+++ src/pageDetailsArticle/commentaires/senewebCommentaires.js
@@ -3,29 +3,18 @@ import React, { Component } from "react";
 import { Row, Col, Navbar } from "react-materialize";
 import Article from "../../article/senewebArticleDetails";
 import Icon from "react-materialize/lib/Icon";
-import Button from "react-materialize/lib/Button";
 
-class PostsRecents extends Component {
-  state = {
-    article_ids: [1, 2, 3]
-  };
+class Commentaires extends Component {
   render() {
     return (
       <Row>
         <Col s={12}>
           <Navbar className="grey lighten-4 black-text center z-depth-0">
-            POSTS RECENTS
+            COMMENTAIRES
             <Icon className="left">bookmark_border</Icon>
             <Icon className="right">dehaze</Icon>
           </Navbar>
         </Col>
-        {this.state.article_ids.map(id => {
-          return (
-            <Col s={4}>
-              <Article id={id} />
-            </Col>
-          );
-        })}
       </Row>
     );
   }
@@ -33,4 +22,4 @@ class PostsRecents extends Component {
 
 // const mapStateToProps
 
-export default PostsRecents;
+export default Commentaires;
--- src/pageDetailsArticle/senewebPageDetailsArticle.js
+++ src/pageDetailsArticle/senewebPageDetailsArticle.js
@@ -6,12 +6,15 @@ import { connect } from "react-redux";
 
 import Article from "../article/senewebArticleDetails";
 import PostsRecents from "./postsRecents/senewebPostsRecents";
+import Commentaires from "./commentaires/senewebCommentaires";
 
 class PageDetailsArticle extends Component {
   render() {
     return (
       <div>
-        <Article id={this.props.match.params.article_id} />;<PostsRecents />
+        <Article id={this.props.match.params.article_id} />;
+        <PostsRecents />
+        <Commentaires />
       </div>
     );
   }
