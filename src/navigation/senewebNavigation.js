05b1078e3f0b1644b99521a73e421b51ea3d77cd
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-12-15 02:06:14 +0000

    Added simple logo

--- src/navigation/senewebNavigation.js
+++ src/navigation/senewebNavigation.js
@@ -4,9 +4,11 @@ import { Navbar, NavItem, Icon, Link } from "react-materialize";
 
 import { connect } from "react-redux";
 
+import Logo from "../img/logo.png";
+
 const Navigation = ({ categories }) => {
   return (
-    <Navbar className="grey darken-2">
+    <Navbar className="grey darken-2" right brand={<img src={Logo} />}>
       <NavItem href="/">
         <Icon className="left">home</Icon>
       </NavItem>
